a693bdd9d1aba2816dd83218ee8b4a5944791301
daniel <daankorssen@gmail.com>
2018-06-26 21:31:11 +0200

    Created a Gender Selector component

---
+++ src/components/GenderSelector.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="field">
+    <div class="control">
+      <div class="select">
+        <select>
+          <option value="" selected disabled hidden>Select your gender</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GenderSelector'
+}
+</script>
