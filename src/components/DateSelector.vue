7c76e352f99a12896dbf061cd84f3a29cfb5cf1d
daniel <9087597+daankrs@users.noreply.github.com>
2018-06-29 22:10:00 +0200

    Made date selector reactive

--- src/components/DateSelector.vue
+++ src/components/DateSelector.vue
@@ -2,9 +2,9 @@
 <div class="field has-addons">
   <div class="control">
       <div class="select">
-        <select>
-          <option  selected disabled hidden>Day</option>
-          <option v-for="day in days" v-bind:key="day">
+        <select v-model="birthday">
+          <option value="0" selected disabled>Day</option>
+          <option v-for="day in days" v-bind:key="day" v-bind:value="day">
             {{ day }}
           </option>
         </select>
@@ -12,9 +12,9 @@
   </div>
   <div class="control">
       <div class="select">
-        <select>
-          <option value="" selected disabled hidden>Month</option>
-          <option v-for="month in months" v-bind:key="month">
+        <select v-model="birthmonth">
+          <option value="0" selected disabled>Month</option>
+          <option v-for="(month,index) in months" v-bind:key="index" v-bind:value="index+1">
             {{ month }}
           </option>
         </select>
@@ -22,9 +22,9 @@
   </div>
   <div class="control">
       <div class="select">
-        <select>
-          <option value="" selected disabled hidden>Year</option>
-          <option v-for="year in years" v-bind:key="year">
+        <select v-model="birthyear">
+          <option value="0" selected disabled>Year</option>
+          <option v-for="year in years" v-bind:key="year" v-bind:value="year">
             {{ year }}
           </option>
         </select>
@@ -53,6 +53,32 @@ export default {
     // Create an array with the last 130 years from this year
     var currentYear = Moment().format('YYYY')
     this.years = Array.from(new Array(130), (val, index) => currentYear - (index + 1))
+  },
+  computed: {
+    birthday: {
+      get () {
+        return this.$store.state.UserData.birth.day
+      },
+      set (birthday) {
+        this.$store.commit('setBirthday', birthday)
+      }
+    },
+    birthmonth: {
+      get () {
+        return this.$store.state.UserData.birth.month
+      },
+      set (birthmonth) {
+        this.$store.commit('setBirthmonth', birthmonth)
+      }
+    },
+    birthyear: {
+      get () {
+        return this.$store.state.UserData.birth.year
+      },
+      set (birthyear) {
+        this.$store.commit('setBirthyear', birthyear)
+      }
+    }
   }
 }
 </script>
