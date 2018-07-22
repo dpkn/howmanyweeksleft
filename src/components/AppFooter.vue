e763931c032ee2c517e014568696fc61280fb023
daniel <9087597+daankrs@users.noreply.github.com>
2018-07-22 13:28:11 +0200

    Fix color change on navigation

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,12 +1,22 @@
 <template>
-  <footer class="is-size-7">
+  <footer id="footer" class="is-size-7">
     <p><a target="_blank" href="https://daankorssen.nl">Made by Daniël</a> | <router-link to="about">About this project</router-link></p>
   </footer>
 </template>
 
 <script>
 export default {
-  name: 'AppFooter'
+  name: 'AppFooter',
+  watch: {
+    '$route' (to, from) { // When route changes, apply the corresponding gradient to the background canvas
+      console.log(to)
+      if (to.name === 'Weeks') {
+        document.getElementById('footer').style.color = '#000'
+      } else {
+        document.getElementById('footer').style.color = '#fff'
+      }
+    }
+  }
 }
 </script>
 
@@ -19,12 +29,13 @@ footer{
   bottom: 5px;
   display: block;
   color:#fff;
+  transition: color 1s;
 }
 a,a:hover,a:visited{
   color:inherit;
   text-decoration: underline;
 }
-@media only screen and (max-width: 769px){
+@media only screen and (max-width: 770px){
   footer {
     position: relative;
     padding: 2em;
